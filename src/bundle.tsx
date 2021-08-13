0d2ec47f2cf1a24ddb8dd7d48bd2b55ff8a2018f
Jonatan Sundqvist <jonatan.sundqvist@vimla.se>
2021-08-13 12:04:00 +0200

    Initial commit

---
+++ src/bundle.tsx
@@ -0,0 +1,11 @@
+import { h, Component, render } from 'preact'
+      
+// Create your app
+const App = () => (
+    <div>
+        <p>Hello World!</p>
+        <p>Isn't this cool?</p>
+    </div>
+)
+
+render(App, document.body)
